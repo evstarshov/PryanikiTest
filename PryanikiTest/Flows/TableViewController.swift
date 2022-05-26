b739f541c0e339f0fca3fc4ec0948e5774ac978d
Crocodil <evstarshov@icloud.com>
2022-05-26 22:03:56 +0300

    comments

--- PryanikiTest/Flows/TableViewController.swift
+++ PryanikiTest/Flows/TableViewController.swift
@@ -14,6 +14,8 @@ class TableViewController: UITableViewController {
     private let networkService = NetworkService()
     private var model: [CellModel] = []
     
+    // MARK: Lifecycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getData()
