71b3128e9220aacad0c5966c42b7f31f490a8c96
Crocodil <evstarshov@icloud.com>
2022-05-26 18:48:29 +0300

    View Controller class

--- PryanikiTest/API/NetworkService.swift
+++ PryanikiTest/API/NetworkService.swift
@@ -13,10 +13,10 @@ final class NetworkService {
     let url = "https://pryaniky.com/static/json/sample.json"
     
     public func makeRequest(completion: @escaping([Datum])->()) {
-        AF.request(url, method: .get).responseJSON { response in
+        AF.request(url, method: .get).responseData { response in
             guard let data = response.data else { return }
             print(String(decoding: response.data!, as: UTF8.self))
-            DispatchQueue.global().async {
+            DispatchQueue.main.async {
                 do {
                     let welcomeJson = try JSONDecoder().decode(Welcome.self, from: data)
                     let dataItems: [Datum] = welcomeJson.data
--- PryanikiTest/Flows/ViewController.swift
+++ PryanikiTest/Flows/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textBlock: UILabel!
+    @IBOutlet weak var picker: UIPickerView!
+    
+    let networkService = NetworkService()
+    var items: [Datum] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        picker.dataSource = self
+        picker.delegate = self
+        getData()
     }
-
-
+    
+    private func getData() {
+        networkService.makeRequest { [weak self] response in
+            guard let self = self else { return }
+            self.items = response
+            print(self.items)
+            print(self.items.count)
+            self.setData()
+        }
+        
+    }
+    
+    private func setData() {
+        textBlock.text = items.first?.data.text
+        if let imageURL = URL(string: items[1].data.url ?? "https://s.mxmcdn.net/images-storage/albums4/2/7/1/2/7/7/34772172_800_800.jpg") {
+            self.imageView.loadImage(url: imageURL)
+        }
+    }
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        items.last?.data.variants?.count ?? 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return items.last?.data.variants?[row].text
+    }
+    
+}
